3bf6ad3a73cf0ee954a2af085884e16b21b2cbcd
Karl D <multicelldesign@gmail.com>
2015-11-22 18:05:06 +0000

    new gigs

--- src/js/jquery.load.js
+++ src/js/jquery.load.js
@@ -48,7 +48,7 @@
 
         var gigSwiper = new Swiper('#gigSwiper', {
             a11y: true,
-            initialSlide: 23,
+            initialSlide: 24,
             pagination: '.swiper-pagination',
             slidesPerView: 2,
             paginationClickable: true,
